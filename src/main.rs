714aa0c6c33b14c26ae9c8a8f5e1d80b3d0c7d7d
Spiryd <neumann.maks@gmail.com>
2023-11-13 12:04:33 +0100

    further setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use dialoguer::{theme::ColorfulTheme, Select};
+
+use crate::collector::Collector;
+
+mod collector;
+
 fn main() {
-    println!("Hello, world!");
+    println!("WELCOME TO LEPRECHAUN");
+    let selections = &[
+        "Collect Data",
+        "Learn",
+        "BackTest",
+        "Deploy",
+        "Exit"
+    ];
+    loop {
+        let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Choose your action: ")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+        match selection {
+            0 => {Collector{};},
+            1 => {},
+            2 => {},
+            3 => {},
+            _ => break,
+        }
+    }
 }
