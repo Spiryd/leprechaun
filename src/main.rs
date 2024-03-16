26d5c1a09fdf0e96ab5ad757db62a38344b4acdd
Spiryd <neumann.maks@gmail.com>
2024-03-16 17:11:42 +0100

    implemented struct for ohlcv data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 
+mod models;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("WELCOME TO LEPRECHAUN");
@@ -7,7 +9,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let vantage_alpha_key = env::var("VANTAGE_ALPHA_KEY").expect("VANTAGE_ALPHA_KEY not found in .env file");
     println!("VANTAGE_ALPHA_KEY: {:?}", vantage_alpha_key);
     let client = reqwest::Client::new();
-    let rsp = client.get("https://www.alphavantage.co/query?").query(&[("function", "TIME_SERIES_DAILY"), ("symbol", "IBM"), ("outputsize", "full"), ("apikey", &vantage_alpha_key)]).send().await?;
-    print!("{:?}", rsp.text().await?);
+    let rsp = client.get("https://www.alphavantage.co/query?").query(&[("function", "TIME_SERIES_DAILY"), ("symbol", "IBM"), ("outputsize", "full"), ("apikey", &vantage_alpha_key),("datatype", "csv")]).send().await?;
+    let body = rsp.text().await?;
+    let mut rdr = csv::Reader::from_reader(body.as_bytes());
+    for result in rdr.deserialize() {
+        let record: models::OHLCV = result?;
+        println!("{:?}", record);
+    }
     Ok(())
 }
---
+++ src/models.rs
@@ -0,0 +1,26 @@
+use serde::{Deserialize, Serialize, Deserializer};
+use chrono::{DateTime, NaiveDate, Utc};
+
+const DATE_FORMAT: &str = "%Y-%m-%d";
+
+#[derive(Debug, Deserialize)]
+pub struct OHLCV {
+    #[serde(rename = "timestamp")]
+    #[serde(deserialize_with = "date_string_to_datetime")]
+    time_stamp: DateTime<Utc>,
+    open: f64,
+    high: f64,
+    low: f64,
+    close: f64,
+    volume: u64,
+}
+
+pub fn date_string_to_datetime<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let nd = NaiveDate::parse_from_str(&s, DATE_FORMAT).unwrap();
+    Ok(DateTime::<Utc>::from_naive_utc_and_offset(nd.and_hms_opt(0, 0, 0).unwrap(), Utc))
+
+}
